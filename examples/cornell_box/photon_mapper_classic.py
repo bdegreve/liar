3d71805a5cad1f689d118b09e5f105fe7f3b2e86
Bram de Greve <bram.degreve@bramz.net>
2006-11-15 19:47:28 +0000

    git-svn-id: https://svn.code.sf.net/p/liar/code/trunk/liar@43 b153b6fe-5711-0410-bcb9-8978480c73f3

---
+++ examples/cornell_box/photon_mapper_classic.py
@@ -0,0 +1,51 @@
+# Demonstration of the classic cornell box using the PhotonMapper
+#
+# LiAR isn't a raytracer
+# Copyright (C) 2004-2006  Bram de Greve
+
+from liar import *
+import geometry
+
+if True:
+	width = 800
+	height = 800
+	super_sampling = 4
+	global_size = 200000
+	caustics_quality = 1
+	raytrace_direct = True
+	gather_rays = 36
+else:
+	width = 400
+	height = 400
+	super_sampling = 1
+	global_size = 10000
+	caustics_quality = 1
+	raytrace_direct = True
+	gather_rays = 9
+
+camera = geometry.getCamera()
+walls = geometry.getWalls()
+blocks = geometry.getBlocks()
+lights = geometry.getLights()
+
+photonMapper = tracers.PhotonMapper()
+photonMapper.maxNumberOfPhotons = 10000000
+photonMapper.globalMapSize = global_size
+photonMapper.causticsQuality = caustics_quality
+photonMapper.isVisualizingPhotonMap = False
+photonMapper.isRayTracingDirect = raytrace_direct
+photonMapper.numFinalGatherRays = gather_rays
+photonMapper.ratioPrecomputedIrradiance = 0.25
+
+image = output.Image("photon_mapper_classic.hdr", (width, height))
+display = output.Display("Classic Cornell box with PhotonMapper", (width, height))
+
+engine = RenderEngine()
+engine.tracer = photonMapper
+engine.sampler = samplers.Stratifier((width, height), super_sampling)
+engine.sampler.jittered = False
+engine.scene = scenery.List(walls + blocks +  lights)
+engine.camera = camera
+engine.target = output.Splitter([image, display])
+engine.render()
+
