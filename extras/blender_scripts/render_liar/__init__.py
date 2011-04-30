9a06975d7d424c79d194c227633e9cdd84493195
Bram de Greve <bram.degreve@bramz.net>
2011-04-30 13:07:16 +0000

    starting port to Blender 2.5 series.
    git-svn-id: https://svn.code.sf.net/p/liar/code/trunk/liar@178 b153b6fe-5711-0410-bcb9-8978480c73f3

---
+++ extras/blender_scripts/render_liar/__init__.py
@@ -0,0 +1,91 @@
+# LiAR isn't a raytracer
+# Copyright (C) 2004-2011  Bram de Greve (bramz@users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# http://liar.bramz.net/
+
+
+# http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Guidelines/Addons
+bl_info = {
+    "name": "LiAR",
+    "description": "LiAR integration",
+    "author": "Bram de Greve",
+    "version": (0, 0, 1),
+    "blender": (2, 5, 7),
+    "api": 35622,
+    "location": "Render > Engine > LiAR",
+    "warning": "alpha alert!",
+    "category": "Render"
+    }
+
+import bpy
+import extensions_framework
+
+def liar_log(str, popup=False):
+    extensions_framework.log(str, popup=popup, module_name="render_liar") 
+
+LiarAddon = extensions_framework.Addon(bl_info)
+register, unregister = LiarAddon.init_functions()
+
+@LiarAddon.addon_register_class
+class RENDERENGINE_liar(bpy.types.RenderEngine):
+    '''
+    LiAR Engine Exporter/Integration class
+    '''
+    
+    bl_idname			= LiarAddon.BL_IDNAME
+    bl_label			= 'LiAR'
+    bl_use_preview		= False
+    
+    def render(self, scene):
+        scale = scene.render.resolution_percentage / 100.0
+        self.size_x = int(scene.render.resolution_x * scale)
+        self.size_y = int(scene.render.resolution_y * scale)
+
+        if scene.name == 'preview':
+            liar_log("render preview not supported")
+            return
+        else:
+            self.__render_scene(scene)
+
+    def __render_scene(self, scene):
+        from render_liar import export
+        path = export.export_scene(scene)
+    
+        pixel_count = self.size_x * self.size_y
+
+        # The framebuffer is defined as a list of pixels, each pixel
+        # itself being a list of R,G,B,A values
+        blue_rect = [[0.0, 0.0, 1.0, 1.0]] * pixel_count
+
+        # Here we write the pixel values to the RenderResult
+        result = self.begin_result(0, 0, self.size_x, self.size_y)
+        layer = result.layers[0]
+        layer.rect = blue_rect
+        self.end_result(result)
+
+# RenderEngines also need to tell UI Panels that they are compatible
+# Otherwise most of the UI will be empty when the engine is selected.
+# In this example, we need to see the main render image button and
+# the material preview panel.
+from bl_ui import properties_render
+properties_render.RENDER_PT_render.COMPAT_ENGINES.add(LiarAddon.BL_IDNAME)
+del properties_render
+
+from bl_ui import properties_material
+properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.add(LiarAddon.BL_IDNAME)
+del properties_material
+
