42c0d017463c0bc238e3fb5420db94bf46c397a0
Bram de Greve <bram.degreve@bramz.net>
2012-06-13 18:38:42 +0000

    adding untested example for goursat object
    git-svn-id: https://svn.code.sf.net/p/liar/code/trunk/liar@202 b153b6fe-5711-0410-bcb9-8978480c73f3

---
+++ examples/scenery/tangle_cube.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+
+# Demonstration of the classic cornell box using the PhotonMapper
+#
+# LiAR isn't a raytracer
+# Copyright (C) 2004-2010  Bram de Greve (bramz@users.sourceforge.net)
+# http://liar.bramz.net/
+
+from liar import *
+from liar.tools import cornell_box, scripting
+
+#if False:
+#	width = 800
+#	height = 800
+#	super_sampling = 4
+#	global_size = 200000
+#	caustics_quality = 1
+#	gather_rays = 36
+#else:
+#	width = 400
+#	height = 400
+#	super_sampling = 1
+#	global_size = 10000
+#	caustics_quality = 1
+#	raytrace_direct = True
+#	gather_rays = 9
+
+options = scripting.renderOptions(
+    width=400,
+    height=400,
+    super_sampling=1,
+    global_size = 20000,
+    caustics_quality=1.,
+    gather_rays = 36
+    )
+
+raytrace_direct = True
+
+photonMapper = tracers.PhotonMapper()
+photonMapper.maxNumberOfPhotons = 10000000
+photonMapper.globalMapSize = options.global_size
+photonMapper.causticsQuality = options.caustics_quality
+photonMapper.isVisualizingPhotonMap = False
+photonMapper.isRayTracingDirect = raytrace_direct
+photonMapper.numFinalGatherRays = options.gather_rays
+photonMapper.ratioPrecomputedIrradiance = 0.25
+
+objects = cornell_box.walls() + cornell_box.lights()
+
+goursat = scenery.Goursat(0, -5, +11.8)
+goursat.useSphereTracing = False
+goursat.shader = shaders.AshikhminShirley()
+goursat.shader.diffuse = textures.Constant(rgb(0.1, 0.1, 0.9))
+goursat.shader.specular = textures.Constant(0.05)
+goursat.shader.specularPowerU = goursat.shader.specularPowerV = textures.Constant(1000)
+objects.append(goursat)
+
+
+engine = RenderEngine()
+engine.tracer = photonMapper
+engine.sampler = samplers.Stratifier((options.width, options.height), options.super_sampling)
+engine.sampler.jittered = False
+engine.scene = cornell_box.scene()
+engine.camera = cornell_box.camera()
+engine.target = scripting.makeRenderTarget(options.width, options.height, "photon_mapper_classic.hdr", "Classic Cornell box with PhotonMapper")
+engine.render()
+
