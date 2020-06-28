4a081a6f9ca467c27aff4f557362d1b33bb76951
Bram de Greve <bram.degreve@bramz.net>
2020-06-28 19:01:18 +0200

    Make tools.download Python 3 compliant

--- src/tools/download.py
+++ src/tools/download.py
@@ -1,11 +1,11 @@
 # LiAR isn't a raytracer
-# Copyright (C) 2004-2010  Bram de Greve (bramz@users.sourceforge.net)
+# Copyright (C) 2004-2020  Bram de Greve (bramz@users.sourceforge.net)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -17,13 +17,20 @@
 #
 # http://liar.bramz.net/
 
+import os
+from urllib.request import urlopen
+
+
+__all__ = ["download"]
+
+
 def download(url, fname=None):
-	import os
-	import urllib
-	fname = fname or os.path.basename(url)
-	if not os.path.exists(fname):
-		print("Downloading '%s' ..." % url)
-		open(fname, 'wb').write(urllib.urlopen(url).read())
-	return fname
+    fname = fname or os.path.basename(url)
+    if not os.path.exists(fname):
+        print("Downloading '{}' ...".format(url))
+        with open(fname, 'wb') as fp, urlopen(url) as res:
+            fp.write(res.read())
+    return fname
+
 
 # EOF
